9533305b761e006ba8aff9aab5ba4bffbaa0789c
Alex Kehayias <alex.kehayias@gmail.com>
2019-02-19 21:01:18 -0800

    Add initial PWA template

---
+++ cli/src/templates/serviceworker.js
@@ -0,0 +1,45 @@
+// TODO this should be a unique key based on version
+var cacheName = 'pwa-test';
+var appShellFiles = [
+  '/index.html',
+];
+
+// On install, download files to the cache
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+// Offline uses cache
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+// Clear out existing cache
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
