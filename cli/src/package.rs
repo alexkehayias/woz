9a73af13fbf95341de376b367cd237fcb613112a
Alex Kehayias <alex.kehayias@gmail.com>
2019-03-22 22:19:56 -0700

    Add function to generate icons from a source image

--- cli/src/package.rs
+++ cli/src/package.rs
@@ -4,7 +4,48 @@ use std::path::PathBuf;
 use failure::Error;
 use failure::ResultExt;
 
-use crate::config::Lib;
+use image;
+use image::DynamicImage;
+use image::GenericImageView;
+use image::imageops::FilterType;
+
+use crate::config::{Config, Lib};
+
+
+/// Generates a set of icon images from the source file
+fn icons(source_file: PathBuf) -> Result<Vec<DynamicImage>, Error> {
+    let src_img = image::open(source_file).unwrap();
+    let src_img_width = src_img.width();
+    let src_img_height = src_img.height();
+    let x = src_img_width / 2;
+    let y = src_img_height / 2;
+
+    let img_dimensions = vec![
+        (192, 192),
+        (168, 168),
+        (144, 144),
+        (96, 96),
+        (72, 72),
+        (48, 48),
+    ];
+
+    let images = img_dimensions.iter().map(|(width, height)| {
+        // TODO how much should we scale the image to make the icon?
+        let scale_width = src_img_width + width;
+        let scale_height = src_img_height + height;
+        src_img.clone()
+            .resize(scale_width, scale_height, FilterType::Nearest)
+            .crop(x, y, *width, *height)
+    }).collect();
+    Ok(images)
+}
+
+#[test]
+fn icons_work() {
+    let result = icons(PathBuf::from("resources/test-src.png"))
+        .expect("Failed to generate icons");
+    result[0].save("resources/crop.png").unwrap();
+}
 
 pub struct WasmPackage {
     pub lib: Lib,
